e48a52ce27ea65a619b89e32f2aebdbcc6a0cbc5
iagosoares95 <iagosoaressantos@gmail.com>
2019-04-02 13:56:16 -0300

    Atualizando

--- framing.py
+++ framing.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from enum import Enum
+import crc
 
 class Framing:
 
@@ -9,35 +10,68 @@ class Framing:
         self.max_bytes=maxbytes
         self.state=Enum('States','idle rx esc')
         self.estado=self.state.idle
+        self.message=bytearray()
 
     def send(self,msg):
-        self.serial.write(msg)
+        finalmsg=bytearray()
+        sendmsg=bytearray()
+
+        fcs=crc.CRC16(msg)
+        msgcrc=fcs.gen_crc()
+
+        for i in range (0,len(msgcrc)):
+            if((msgcrc==0x7E) or (msgcrc==0x7D)):
+                finalmsg=finalmsg+b'\x7D'+bytes([self._xor20(msgcrc[i])])
+            else:
+                finalmsg=finalmsg+bytes([msgcrc[i]])
+
+        sendmsg=b'\x7E'+finalmsg+b'\x7E'
+
+        self.serial.write(sendmsg)
 
     def receive(self):
         while(true):
-            byte=self.serial.read()
+            msgreceived=self.serial.read()
 
 
-    def handlei(self,received):
+
+
+    def handle(self,received):
         if(self.estado==self.state.idle):
             self.estado=self.waiting(received)
             return false
 
-        if(self.estado==self.state.rx):
+        elif(self.estado==self.state.rx):
             self.estado=self.reception(received)
             return false
 
-        if(self.estado==self.state.esc):
+        elif(self.estado==self.state.esc):
             self.estado=self.escape(received)
             return false
 
+        else:
+            self.estado=self.
+
+
     def waiting(self,received):
+        if(received==b'\x7E'):
+            self.message=bytearray()
+            return self.States.rx
+        else:
+            return self.States.idle
 		
     def reception(self,received):
+        if(received==b\'x7E'):
+            return self.States.rx
+        elif(received!=b\'x7E' and received!=b\'x7D'):
+            self.message=self.message+received
+            return
+        elif(received==b\'x7D'):
+            return self.States.esc
 
     def escape(self,received):
         if(self.received == )
 
     def xor(self,received):
         received1=received^0x20
-        return received
+        return received1
