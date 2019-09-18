5fc555bb032c8420f1ada822493833668829e058
iagosoares95 <iagosoaessantos@gmail.com>
2019-09-18 10:48:15 -0300

    terminndo tun

--- arq.py
+++ arq.py
@@ -6,4 +6,17 @@ from enum import Enum
 class Arq:
     def __init__(self, framing):
         self.fra = framing
-        self.States = Enum('')
+        self.States = Enum('States', 'idle waiting')
+        self.state = self.States.idle
+        self.Eventtype = Enum('EventType', 'payload frame timeout')
+
+    def send(self, data):
+        self.fra.send()
+
+    def receive(self):
+        self.fra.received()
+
+    def handle(self):
+        if(self.state == self.States.idle):
+
+        elif(self.state == self.States.waiting):
--- link.py
+++ link.py
@@ -3,6 +3,7 @@
 from tun import Tun
 import framing
 import serial
+import poller
 
 class Link:
     def __init__(self, serial_port, ip1, ip2):
@@ -12,17 +13,18 @@ class Link:
         self.tun.start()
         self.pol = poller.Poller()
         self.tun_callback = TunCallback(self.tun,self)
-        self.adiciona(self.tun.Callback)
+        self.pol.adiciona(self.tun_callback)
         self.serial_callback = SerialCallback(self)
-        self.adiciona(self.serial_callback)
+        self.pol.adiciona(self.serial_callback)
         self.pol.despache()
 
     def send(self, data):
+        print("Dado: ", data)
         self.fra.send(data)
 
     def received(self):
         buffer = self.fra.receive()
-        if((type(buf)==bytearray) and buf!=bytearray()):
+        if((type(buffer) == bytearray) and buffer != bytearray()):
             print('Pacote recebido:')
             print(buffer)
             self.tun.send_frame(buffer, Tun.PROTO_IPV4)
@@ -30,13 +32,21 @@ class Link:
 class TunCallback(poller.Callback):
     def __init__(self, tun, link):
         poller.Callback.__init__(self, tun.fd)
-        self.tun = tun
-        self.lin = lin
+        self.tun1 = tun
+        self.link = link
 
-    def handle()
+    def handle(self):
+        proto, payload = self.tun1.get_frama()
+        print("Lido: ", payload)
+        self.link.send(payload)
 
 class SerialCallback(poller.Callback):
     def __init__(self,link):
-        poller.Callback.__init__(self, lin.ser)
+        poller.Callback.__init__(self, link.ser)
+        self.link = link
+        self.ser1 = link.ser
 
     def handle(self):
+        recv_data = self.ser1.read()
+        if(recv_data != bytearray()):
+            self.ser1.receive()
