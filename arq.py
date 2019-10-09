62bdd64bcdb1ec1f216420f09b5d12da0d5639e5
iagosoares95 <iagosoaessantos@gmail.com>
2019-10-09 06:36:19 -0300

    começando sessão

--- arq.py
+++ arq.py
@@ -5,18 +5,43 @@ from enum import Enum
 
 class Arq:
     def __init__(self, framing):
-        self.fra = framing
+        self.fra = framingz
         self.States = Enum('States', 'idle waiting')
         self.state = self.States.idle
         self.Eventtype = Enum('EventType', 'payload frame timeout')
+        self.event = None
+        self.buffer = bytearray()
 
     def send(self, data):
-        self.fra.send()
+        if(data == bytearray()):
+            return
+        self.event = self.Eventtype.payload
+        self.data = data
+        self.handle_state(self.event)
 
     def receive(self):
-        self.fra.received()
+        self.buffer = self.fra.received()
+        if(self.bufer == bytearray()):
+            return bytearray()
+        
+        self.event = self.Eventtype.frame
+        self.handle_state(self.event)
+        return self.buffer
 
-    def handle(self):
+    def handle_state(self, event):
         if(self.state == self.States.idle):
-
+            self.state = self.idle_func(event)
+            return self.state
         elif(self.state == self.States.waiting):
+            self.state = self.waiting_func(event)
+            return self.state
+
+    def idle_func(self, event):
+        if(self.event == self.Eventtype.payload):
+        
+        elif(self.event == self.Eventtype.frame):
+
+    def waiting_func(self, event):        
+        if(self.event == self.Eventtype.payload):
+        
+        elif(self.event == self.Eventtype.frame):
---
+++ session.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from enum import Enum
+import ARQ
+import link
+
+class Session:
+    def __init__(self, arq):
+        self.States = Enum('States', 'disc hand1 hand2 hand3 con check half1 half2')
+        self.states = self.State.disc
+        self.arq = arq
+        self.CR = b'\x00'
+        self.CC = b'\x01'
+        self.KR = b'\x02'
+        self.KC = b'\x03'
+        self.DR = b'\x04'
+        self.DC = b'\x05'
+        self.start = False
+        self.recv_data = bytearray()
+        self.proto = b'\xff'
+
+    def begin(self):
+        if(self.states == seld.States.con):
+            return
+        if(self.start == True):
+            return
+        self.states = self.States.disc
+        self.start = True
+        self.handle()
+    
+    def finish(self):
+        self.DR_func()
+        seld.states = self.States.half1
+
+    def send(self):
+
+    def receive(self):
+        self.recv_data = self.arq.receive()
+        if(self.handle() == False):
+
+    def handle(self):
+        if(self.states == self.States.disc):
+            self.states = self.disc_func()
+            return False
+        elif(self.states == self.States.hand1):
+            self.states = self.hand1_func()
+            return False
+        elif(self.states == self.States.hand2):
+            self.states = self.hand2_func()
+            return False
+        elif(self.states == self.States.hand3):
+            self.states = self.hand3_func()
+            return False
+        elif(self.states == self.States.con):
+            self.states = self.con_func()
+            return False
+        elif(self.states == self.States.check):
+            self.states = self.check_func()
+            return False
+        elif(self.states == self.States.half1):
+            self.states = self.half1_func()
+            return False
+        elif(self.states == self.States.half2):
+            self.states = self.half2_func()
+            return False
+
+    def disc_func(self):
+        if(len(self.recv_data) == 0):
+            return self.States.disc
+        if(self.start == True):
+            self.CR_func()
+            return self.States.hand1
+        else:
+            if()
+
+    def hand1_func(self):
+        if(len(self.recv_data) == 0):
+            self.CR_func()
+            return self.States.hand1
+        if()            
+
+    def hand2_func(self):
+        if(len(self.recv_data) == 0):
+            return self.States.hand2
+        if()            
+
+    def hand3_func(self):
+
+    def con_func(self):
+
+    def check_func(self):
+
+    def half1_func(self):
+        if(len(self.recv_data) == 0):
+            self.DR_func()
+            return self.States.half1
+        if()
+
+    def half2_func(self):
+
+    def CR_func(self):
+        send_data = self.proto + sel.CR
+        self.arq.send(send_data)
+    
+    def DR_func(self):
+        send_data = self.proto + self.DR
+        self.arq.send(send_data)
