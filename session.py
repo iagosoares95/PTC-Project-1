e795b811840cf9c37685f340c67e69212dbffc5c
iagosoares95 <iagosoaessantos@gmail.com>
2019-10-31 08:24:51 -0300

    mechendo na sessão

--- session.py
+++ session.py
@@ -9,7 +9,7 @@ import time
 class Session:
     def __init__(self, arq, timeout):
         self.States = Enum('States', 'disc hand1 hand2 hand3 con check half1 half2')
-        self.states = self.State.disc
+        self.states = self.States.disc
         self.arq = arq
         self.CR = b'\x00'
         self.CC = b'\x01'
@@ -22,6 +22,7 @@ class Session:
         self.proto = b'\xff'
         self.s_data = bytearray()
         self.l_time = time.time()
+        self.timeout = timeout
 
     def begin(self):
         if(self.states == seld.States.con):
@@ -42,13 +43,18 @@ class Session:
         else: 
             return False
 
-    def send(self):
+    def send(self, data):
         if(self.states != self.States.con):
             return 
+        self.s_data = bytearray()
+        self.s_data = self.proto + data
 
     def receive(self):
         self.recv_data = self.arq.receive()
         if(self.handle() == False):
+            return self.recv_data
+        else:
+            return bytearray()
 
     def handle(self):
         if(self.states == self.States.disc):
@@ -71,9 +77,13 @@ class Session:
             return False
         elif(self.states == self.States.half1):
             self.states = self.half1_func()
+            if(self.states == self.States.disc):
+                return True
             return False
         elif(self.states == self.States.half2):
             self.states = self.half2_func()
+            if(self.states == self.States.disc):
+                return True
             return False
 
     def disc_func(self):
@@ -81,49 +91,99 @@ class Session:
             return self.States.disc
         if(self.start == True):
             self.CR_func()
+            self.l_time = time.time()
             return self.States.hand1
         else:
-            if():
+            if(len(self.recv_data) == 0):
+                return self.States.disc
+            if((self.recv_data[2:3] == self.proto[0:1]) and (self.recv_data[3:4] == self.CR[0:1])):
+                print("Pedido de conexão recebido")
+                self.s_data = bytearray()
+                self.s_data = self.proto + self.CC
+                print("Enviando CC")
+                self.arq.send(self.s_data)
+                return self.States.hand2
+            return self.States.disc
 
     def hand1_func(self):
         if(len(self.recv_data) == 0):
-            self.CR_func()
             return self.States.hand1
-        if()            
+        if(self.recv_data[3:4] == self.CC[0:1]):
+            print("CC recebido")
+            return self.States.hand3
+        return self.States.hand1              
 
     def hand2_func(self):
         if(len(self.recv_data) == 0):
             return self.States.hand2
-        if():           
+        if(self.):           
 
     def hand3_func(self):
+
 
     def con_func(self):
         if(self.s_data != bytearray()):
             self.arq.send(s_data)
             return self.States.con
+        if(len(self.recv_data) != 0):
+            if((self.recv_data[2:4] == self.proto[0:1]) and (self.recv_data[3:4] == self.DR[0:1])):
+                print("Pedido de desconexão recebido")
+                self.DR_func()
+                return self.States.half2
+            else:
+                self.recv_data = self.recv_data[4:]
+                return self.States.con
+        return self.States.con
             
     def check_func(self):
 
     def half1_func(self):
         if(len(self.recv_data) == 0):
-            self.DR_func()
             return self.States.half1
-        if():
+        if(self.recv_data[3:4] == self.DR[0:1]):
+            print("Confirmação de desconexão recebida")
+            self.s_data = bytearray()
+            self.s_data = self.proto + self.DC
+            print("Enviando confirmaçãoo de desconexão")
+            self.arq.send(self.s_data)
+            return self.States.disc
+        return self.States.half1
 
     def half2_func(self):
         if(len(self.recv_data) == 0):
             return self.States.disc
-        if():
+        if(self.recv_data[3:4] == self.DC[0:1]):
+            print("Confirmação de desconexão recebida")
+            return self.States.disc
+        if(self.recv_data[3:4] == self.DR[0:1]):
+            self.DR_func()
+            return self.States.half2
 
     def CR_func(self):
         send_data = self.proto + sel.CR
+        self.l_time = time.time()
         self.arq.send(send_data)
     
     def DR_func(self):
         send_data = self.proto + self.DR
+        self.l_time = time.time()
         self.arq.send(send_data)
 
     def timeout_func(self):
         if(self.states == self.States.hand1):
-            self.diff_time = time.time() - self
+            self.diff_time = time.time() - self.l_time
+            if(diff_time > self.timeout):
+                self.CR_func()
+        if(self.states == self.States.hand2):
+            self.diff_time = time.time() - self.l_time
+            if(diff_time > self.timeout):
+                self.states = self.States.disc
+                self.start = False
+        if(self.states == self.States.half1):
+            self.diff_time = time.time() - self.l_time
+            if(diff_time > self.timeout):
+                self.DR_func()
+        if(self.states == self.States.half2):
+            self.diff_time = time.time() - self.l_time
+            if(diff_time > self.timeout):
+                self.states = self.States.disc
